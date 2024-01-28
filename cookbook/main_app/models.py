c0700c5298e3e74559b519b81b65562e3bc0c55a
Alexandr Toropov <bligs100@gmail.com>
2024-01-28 19:28:02 +0600

    #3 Описаны модели Product, Recipe и смежная таблица для связи "многие-ко-многим", связывающие 2 модели

--- cookbook/main_app/models.py
+++ cookbook/main_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=300, null=False, blank=False, unique=True, verbose_name='Продукт')
+    number_used_in_dish = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.name
+    
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=300, null=False, blank=False, verbose_name='Рецепт')
+    products = models.ManyToManyField(Product, related_name='recipes', through='main_app.RecipeProduct')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class RecipeProduct(models.Model):
+    recipe = models.ForeignKey(Recipe, related_name='recipe_product', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='product_recipe', on_delete=models.CASCADE)
+    weight_grams = models.PositiveIntegerField(null=False, blank=False)
+
+    def __str__(self) -> str:
+        return f"{self.product.name} - ({self.weight_grams}gr)"
