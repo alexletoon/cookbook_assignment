e7a19797efab213af34192d5fa9936f521a2a1d0
Alexandr Toropov <bligs100@gmail.com>
2024-01-28 19:52:29 +0600

    #5 создан super user и админка

--- cookbook/main_app/admin.py
+++ cookbook/main_app/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from main_app.models import Product, Recipe, RecipeProduct
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'number_used_in_dish']
+    list_filter = ['name', 'number_used_in_dish']
+    search_fields = ['name', 'number_used_in_dish']
+    fields = ['name', 'number_used_in_dish']
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    list_filter = ['name']
+    search_fields = ['name']
+    fields = ['name']
+
+
+class RecipeProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'recipe', 'product', 'weight_grams']
+    list_filter = ['recipe', 'product', 'weight_grams']
+    search_fields = ['recipe', 'product', 'weight_grams']
+    fields = ['recipe', 'product', 'weight_grams']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeProduct, RecipeProductAdmin)
